70fc1c849cced1c96f568d680d24d77fd4e729cf
MeitarBach <meitar.bach666@gmail.com>
2019-08-27 18:35:33 +0300

    commit

---
+++ VR_Final_Project_v1/Intermediate/Source/VR_Final_Project_v1.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class VR_Final_Project_v1 : ModuleRules
+{
+	public VR_Final_Project_v1(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
---
+++ VR_Final_Project_v1/Intermediate/Source/VR_Final_Project_v1.cpp
@@ -0,0 +1,4 @@
+#include "CoreTypes.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultModuleImpl, VR_Final_Project_v1, "VR_Final_Project_v1");
